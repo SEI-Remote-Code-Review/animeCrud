50987083031ff37f64da068d1bbc0320b5a125c0
EugeneR <eugenerillo@gmail.com>
2022-03-08 23:05:22 -0500

    heroku push

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,7 @@ const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
   animes: [{type: mongoose.Schema.Types.ObjectId, ref:'Anime'}],
-  reviews:[{type: mongoose.Schema.Types.ObjectId, ref:'Review'}]
+  //reviews:[{type: mongoose.Schema.Types.ObjectId, ref:'Review'}]
 }, {
   timestamps: true
 })
